187a537b27dab8659c3b143f444ef06a3d204d4a
cuong <luvancuong0105@gmail.com>
2026-04-23 23:14:12 +0700

    edit rule log

--- DeviceIOT/DebugInfo.h
+++ DeviceIOT/DebugInfo.h
@@ -1,27 +1,25 @@
 
-
-#define NRF_LOG_INTERNAL_MODULE(level, level_id, ...)                                    \
-    if(level <= NRF_LOG_DEFAULT_LEVEL)                                                \
-    {                                                                                    \
-        if (NRF_LOG_FILTER >= level)                                                     \
-        {                                                                                \
-            LOG_INTERNAL(LOG_SEVERITY_MOD_ID(level_id), __VA_ARGS__);                    \
-        }                                                                                \
-    }
-
-#define NRF_LOG_SEVERITY_DEBUG 3 
-
-#define NRF_LOG_INTERNAL_ERROR(...) \
-                NRF_LOG_INTERNAL_MODULE(1, NRF_LOG_SEVERITY_DEBUG,__VA_ARGS__)
-#define NRF_LOG_INTERNAL_WARNING(...) \
-            NRF_LOG_INTERNAL_MODULE(2, NRF_LOG_SEVERITY_DEBUG,__VA_ARGS__)
-#define NRF_LOG_INTERNAL_INFO(...) \
-        NRF_LOG_INTERNAL_MODULE(3, NRF_LOG_SEVERITY_DEBUG, __VA_ARGS__)
-#define NRF_LOG_INTERNAL_DEBUG(...) \
-        NRF_LOG_INTERNAL_MODULE(4, NRF_LOG_SEVERITY_DEBUG, __VA_ARGS__)
+#define DEVICE_LOG_DEFAULT_LEVEL 4
 
 
-#define NRF_LOG_ERROR(...)                     NRF_LOG_INTERNAL_ERROR(__VA_ARGS__)
-#define NRF_LOG_WARNING(...)                   NRF_LOG_INTERNAL_WARNING( __VA_ARGS__)
-#define NRF_LOG_INFO(...)                      NRF_LOG_INTERNAL_INFO( __VA_ARGS__)
-#define NRF_LOG_DEBUG(...)                     NRF_LOG_INTERNAL_DEBUG( __VA_ARGS__)
+void DEVICE_LOG_INTERNAL_MODULE( int level, const char* log_info){
+    if(level <= DEVICE_LOG_DEFAULT_LEVEL)                                                
+    {                                                                                       
+        Serial.println(log_info);                                                                       \
+    }      
+}
+
+#define DEVICE_LOG_INTERNAL_ERROR(log_info) \
+                DEVICE_LOG_INTERNAL_MODULE(1, log_info)
+#define DEVICE_LOG_INTERNAL_WARNING(log_info) \
+            DEVICE_LOG_INTERNAL_MODULE(2, log_info)
+#define DEVICE_LOG_INTERNAL_INFO(log_info) \
+        DEVICE_LOG_INTERNAL_MODULE(3, log_info)
+#define DEVICE_LOG_INTERNAL_DEBUG(log_info) \
+        DEVICE_LOG_INTERNAL_MODULE(4,log_info)
+
+
+#define DEVICE_LOG_ERROR(log_info)                     DEVICE_LOG_INTERNAL_ERROR(log_info)
+#define DEVICE_LOG_WARNING(log_info)                   DEVICE_LOG_INTERNAL_WARNING(log_info)
+#define DEVICE_LOG_INFO(log_info)                      DEVICE_LOG_INTERNAL_INFO(log_info)
+#define DEVICE_LOG_DEBUG(log_info)                     DEVICE_LOG_INTERNAL_DEBUG(log_info)
